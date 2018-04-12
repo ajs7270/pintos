54856a4d743e1a9bd773c9ad9d4a3a3a355444f1
jisuAN <ajs7270@naver.com>
2018-04-13 03:58:26 +0900

    debug

--- projects/msgpassing/msgpassingtest.c
+++ projects/msgpassing/msgpassingtest.c
@@ -18,10 +18,13 @@ typedef struct mailbox{
 		ex)
 		m == 1 : can send
 		m <= 0 : can't send
+
+		semaphore s for mutual exclustion
 	*/
 		char* data[MAX_SIZE];
 		struct semaphore n;
 		struct semaphore m;
+		struct semaphore s;
 } mail_box;
 
 void msg_send(mail_box* destination, char* message);
@@ -35,32 +38,46 @@ void run_message_passing_test(void)
 {
 	mail_box box;
 	mail_box_init(&box);
-
+	printf("m:%d n:%d s:%d \n",box.m.value,box.n.value,box.s.value);
 	thread_create("sender", PRI_DEFAULT, producer, &box);
 	thread_create("receiver", PRI_DEFAULT, consumer, &box);
 }
 
 void msg_send(mail_box* destination, char* message){
-
+	printf("1 %s m:%d n:%d s:%d \n",thread_current()->name,destination->m.value,destination->n.value,destination->s.value);
 	sema_down(&(destination->m));
+	printf("sema %s m:%d n:%d s:%d \n",thread_current()->name,destination->m.value,destination->n.value,destination->s.value);
+	printf("2 %s m:%d n:%d s:%d \n",thread_current()->name,destination->m.value,destination->n.value,destination->s.value);
+	sema_down(&(destination->s));
+	printf("3 %s",thread_current()->name);
 	strlcpy(destination->data, message,MAX_SIZE);
+	thread_sleep(100);
 	printf("mail box data : %s\n",destination->data);
 	sema_up(&(destination->n));
-	printf("1");
+	sema_up(&(destination->s));
 }
 
 void msg_receive(mail_box* source, char* message){
-	printf("2");
+	printf("4 %s",thread_current()->name);
 	sema_down(&(source->n));
+	printf("5 %s",thread_current()->name);
+	sema_down(&(source->s));
+	printf("6 %s",thread_current()->name);
 	strlcpy(message, source->data,MAX_SIZE);
+	//thread_sleep(100);
 	printf("receive data : %s\n",message);
 	sema_up(&(source->m));
+	sema_up(&(source->s));
 }
 
 void mail_box_init(mail_box* box){
 
-	sema_init(&(box->n),0);//receive semaphore = 1
-	sema_init(&(box->m),1);//send semaphore = 0
+	sema_init(&(box->n),0);//receive semaphore = 0
+	printf("123\n");
+	sema_init(&(box->m),1);//send semaphore = 1
+	printf("12345\n");
+	sema_init(&(box->s),1);// m.e semaphore = 1
+	printf("123456\n");
 }
 
 void producer(mail_box* box){
