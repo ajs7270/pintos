b85d11ff0cf586a31ffe14083d87fa299d254a55
jisu An <ajs7270@naver.com>
2018-04-13 19:26:46 +0900

    move function

--- projects/msgpassing/msgpassingtest.c
+++ projects/msgpassing/msgpassingtest.c
@@ -5,32 +5,6 @@
 #include "threads/thread.h"
 #include "threads/synch.h"
 
-#define MAX_SIZE 20
-
-typedef struct mailbox{
-	/*
-		semaphore n for receive semaphore
-		ex)
-		n == 1 : can receive
-		n <= 0 : can't receive
-
-		semaphore m for send semaphore
-		ex)
-		m == 1 : can send
-		m <= 0 : can't send
-
-		semaphore s for mutual exclustion
-	*/
-		char* data[MAX_SIZE];
-		struct semaphore n;
-		struct semaphore m;
-		struct semaphore s;
-} mail_box;
-
-void msg_send(mail_box* destination, char* message);
-void msg_receive(mail_box* source, char* message);
-void mail_box_init(mail_box* box);
-
 void producer(void* aux);
 void consumer(void* aux);
 
@@ -42,31 +16,6 @@ void run_message_passing_test(void)
 	thread_sleep(100);
 	thread_create("receiver", PRI_DEFAULT, consumer, &box);
 	thread_sleep(10000);
-}
-
-void msg_send(mail_box* destination, char* message){
-	sema_down(&(destination->m));
-	sema_down(&(destination->s));
-	strlcpy(destination->data, message,MAX_SIZE);
-	printf("mail box data : %s\n",destination->data);
-	sema_up(&(destination->s));
-	sema_up(&(destination->n));
-
-}
-
-void msg_receive(mail_box* source, char* message){
-	sema_down(&(source->n));
-	sema_down(&(source->s));
-	strlcpy(message, source->data,MAX_SIZE);
-	sema_up(&(source->s));
-	sema_up(&(source->m));
-}
-
-void mail_box_init(mail_box* box){
-
-	sema_init(&(box->n),0);//receive semaphore = 0
-	sema_init(&(box->m),1);//send semaphore = 1
-	sema_init(&(box->s),1);// m.e semaphore = 1
 }
 
 void producer(void* aux){
