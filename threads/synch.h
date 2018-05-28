9c4c541cb1b2cff46f236f4f09f0fc3ed1afe7f0
jisu An <ajs7270@naver.com>
2018-05-28 19:47:16 +0900

    project #2 setting

--- threads/synch.h
+++ threads/synch.h
@@ -4,35 +4,12 @@
 #include <list.h>
 #include <stdbool.h>
 
-/* mail box max size */
-#define MAX_SIZE 20
-
 /* A counting semaphore. */
-struct semaphore
+struct semaphore 
   {
     unsigned value;             /* Current value. */
     struct list waiters;        /* List of waiting threads. */
   };
-
-typedef struct mailbox{
-	/*
-		semaphore n for receive semaphore
-		ex)
-		n == 1 : can receive
-		n <= 0 : can't receive
-
-		semaphore m for send semaphore
-		ex)
-		m == 1 : can send
-		m <= 0 : can't send
-
-		semaphore s for mutual exclustion
-	*/
-		char* data[MAX_SIZE];
-		struct semaphore n;
-		struct semaphore m;
-		struct semaphore s;
-} mail_box;
 
 void sema_init (struct semaphore *, unsigned value);
 void sema_down (struct semaphore *);
@@ -41,7 +18,7 @@ void sema_up (struct semaphore *);
 void sema_self_test (void);
 
 /* Lock. */
-struct lock
+struct lock 
   {
     struct thread *holder;      /* Thread holding lock (for debugging). */
     struct semaphore semaphore; /* Binary semaphore controlling access. */
@@ -54,7 +31,7 @@ void lock_release (struct lock *);
 bool lock_held_by_current_thread (const struct lock *);
 
 /* Condition variable. */
-struct condition
+struct condition 
   {
     struct list waiters;        /* List of waiting threads. */
   };
@@ -63,10 +40,6 @@ void cond_init (struct condition *);
 void cond_wait (struct condition *, struct lock *);
 void cond_signal (struct condition *, struct lock *);
 void cond_broadcast (struct condition *, struct lock *);
-
-void msg_send(mail_box* destination, char* message);
-void msg_receive(mail_box* source, char* message);
-void mail_box_init(mail_box* box);
 
 /* Optimization barrier.
 
